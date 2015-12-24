591964e231beae09502e5a2fb0b07089aa4a34e0
Cirr <cirrial@gmail.com>
2015-12-24 17:02:12 +0000

    Basic strategy, although lacking any spawning behaviour

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-/**
- * Created by Samuel on 24/12/2015.
- */
+var builder = require('builder');
+var harvester = require('harvester');
+var guard = require('guard');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+
+        if(creep.memory.role == 'harvester') {
+            harvester(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            builder(creep);
+        }
+        if(creep.memory.role == 'guard') {
+            guard(creep);
+        }
+    }
+}
